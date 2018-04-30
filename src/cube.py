27683bee4c205cab237f6500b1ecb591efe3cca9
PAUL <paulvikiraj@gmail.com>
2018-04-30 19:47:47 +0530

    Cube

--- src/cube.py
+++ src/cube.py
@@ -3,7 +3,11 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
 
-VOID, ADD_PLANE, REMOVE_PLANE, MOTION_ON, MOTION_OFF, QUIT = list(range(6))
+VOID, CLEAR,ADD_PLANE, REMOVE_PLANE,COLOR, MOTION_ON, MOTION_OFF, QUIT = list(range(8))
+VOID, RED, GREEN,BLUE =list(range(4))
+
+PROMPT = ("Press keys '1' - '0' to start callbacks",
+          "Press ESCAPE to exit.")
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 0.0)  # Set background color to black and opaque
@@ -12,6 +16,14 @@ def init():
 
 def square():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    w = float(glutGet(GLUT_WINDOW_WIDTH))
+    h = float(glutGet(GLUT_WINDOW_HEIGHT))
+    y = 25.0
+    for s in PROMPT:
+        glRasterPos(40.0, y)
+        y += 30.0
+        for c in s:
+            glutBitmapCharacter(GLUT_BITMAP_8_BY_13, ord(c))
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glTranslatef(0.0,0.0,-20.0)#View changes of the cube can be done here
@@ -115,6 +127,22 @@ def add_plane():
 
     return
 
+def createmenu():
+    submenuid = glutCreateMenu(color)
+    glutAddMenuEntry("Red", RED)
+    glutAddMenuEntry("Blue", BLUE)
+    glutAddMenuEntry("Green", GREEN)
+
+    menu_id = glutCreateMenu(menu)
+    glutAddMenuEntry("Clear",CLEAR)
+    glutAddMenuEntry("Add Cube", ADD_PLANE)
+    glutAddMenuEntry("Remove plane", REMOVE_PLANE)
+    glutAddSubMenu("Background", submenuid)
+    glutAddMenuEntry("Motion", MOTION_ON)
+    glutAddMenuEntry("Quit", QUIT)
+    glutAttachMenu(GLUT_RIGHT_BUTTON)
+
+
 def remove_plane():
     return 0
 
@@ -128,11 +156,41 @@ def doquit():
     sys.exit(0)
     return
 
-menudict = {ADD_PLANE: add_plane,
+def color(item):
+    colordict[item]()
+    return 0
+
+def clear():
+    return
+
+def red():
+    glClearColor(1,0,0,1)
+    glutPostRedisplay()
+    return
+
+def blue():
+    glClearColor(0,0,1,1)
+    glutPostRedisplay()
+    return
+
+def green():
+    glClearColor(0,1,0,1)
+    glutPostRedisplay()
+    return
+
+colordict={RED:red,
+           GREEN:green,
+           BLUE:blue}
+
+menudict = {CLEAR:clear,
+            ADD_PLANE: add_plane,
             REMOVE_PLANE: remove_plane,
+            COLOR:color,
             MOTION_ON: domotion_on,
             MOTION_OFF: domotion_off,
             QUIT: doquit}
+
+
 def reshape(w,h):
     if(h==0):
         h=1
@@ -149,16 +207,11 @@ def menu(item):
 if __name__=="__main__":
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE)
-    glutInitWindowPosition(50, 100)
+    glutInitWindowPosition(50,100)
     glutInitWindowSize(640, 480)
     glutCreateWindow(b'Square')
     glutDisplayFunc(square)
-    glutCreateMenu(menu)
-    glutAddMenuEntry("Add Cube", ADD_PLANE)
-    glutAddMenuEntry("Remove plane", REMOVE_PLANE)
-    glutAddMenuEntry("Motion", MOTION_ON)
-    glutAddMenuEntry("Quit", QUIT)
-    glutAttachMenu(GLUT_RIGHT_BUTTON)
     glutReshapeFunc(reshape)
+    createmenu()
     init()
     glutMainLoop()
